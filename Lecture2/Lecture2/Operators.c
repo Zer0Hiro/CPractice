e32763a86695291ca843c25880e196110ad280fe
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-03-24 19:59:21 +0200

    Lec2

---
+++ Lecture2/Lecture2/Operators.c
@@ -0,0 +1,28 @@
+/*
+
+p || q == OR
+p && q == AND
+
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+int helo(int, int);
+
+int main()
+{
+	helo(5, 15);
+	return 0;
+}
+
+int helo(int i, int k)
+{
+	for (; i < 10;)
+	{
+		printf("Hello:(%d)\n", k);
+		i++;
+	}
+	return 0;
+}
+*/
+
