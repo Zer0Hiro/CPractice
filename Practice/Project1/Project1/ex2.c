ce643ea5f8cbd0e57357cbe721120baf27ab07cd
Zer0Hiro <95877340+Zer0Hiro@users.noreply.github.com>
2025-04-16 20:20:58 +0300

    upd

---
+++ Practice/Project1/Project1/ex2.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define N 6
+
+int distance(char A[], char B[]);
+
+int main()
+{
+	int strA[N], strB[N];
+	
+	// Get first string
+	printf("Enter the first string: ");
+	fgets(strA, N, stdin);
+	rewind(stdin);
+
+	// Get second string
+	printf("Enter the second string: ");
+	fgets(strB, N, stdin);
+	
+	printf("The distance is: %d",distance(strA, strB));
+	return 1;
+}
+
+
+// This function will check if there is a difference between 2 strings and return how many differences was detected
+int distance(char A[], char B[])
+{
+	int i, count = 0;
+
+	// Check if strings have equal length
+	if (strlen(A) != strlen(B)) return -1;
+
+	// Count how many chars different at the same spot in both stings
+	for (i = 0; i < A[i] != '\0'; i++)
+	{
+		if (A[i] != B[i]) count++;
+	}
+	return count;
+}
